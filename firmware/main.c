e0d578ce19ec20de796fed29eea6975aa536033d
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2020-07-17 10:53:33 -0400

    So it turns out that accidentally buying a 24 MHz crystal oscillator works perfectly for running the PIC18F4550 at max speed XD

--- firmware/main.c
+++ firmware/main.c
@@ -2,24 +2,27 @@
 #include <delay.h>
 
 
-#pragma config XINST	= OFF
-#pragma config FOSC 	= INTOSCIO_EC
-#pragma config WDT 		= OFF
-#pragma config LVP 		= OFF // TODO: check this
-#pragma config BOR 		= ON
-#pragma config MCLRE 	= OFF // TODO: check this
-#pragma config PLLDIV 	= 2
-#pragma config CPUDIV   = OSC1_PLL2
-#pragma config FCMEN 	= OFF
-#pragma config IESO 	= OFF
-#pragma config PWRT 	= OFF // TODO: check this
-#pragma config BORV 	= 0
-#pragma config VREGEN 	= OFF // ?
-#pragma config PBADEN 	= OFF // ? 	
+#pragma config FOSC 	= ECPLLIO_EC		// 24 MHz External Clock
+#pragma config PLLDIV 	= 6 				// 24 / 6 = 4 MHz PLL Input
+#pragma config CPUDIV   = OSC1_PLL2 		// 96 / 2 = 48 MHz Core Clock
+#pragma config USBDIV 	= 2					// 96 / 2 = 48 MHz USB Clock
+
+#pragma config XINST	= OFF 				// No Extended ISA
+#pragma config WDT 		= OFF 				// No Watchdog Timer
+#pragma config LVP 		= OFF 				// No Low-Voltage Programming
+#pragma config BOR 		= OFF				// No Brown-out-Reset
+#pragma config MCLRE 	= ON  				// MCLR Reset
+#pragma config FCMEN 	= OFF 				// No Failsafe Clock Monitor
+#pragma config IESO 	= OFF 				// No Internal External Switchover
+#pragma config PWRT 	= ON 				// Power-Up Timer
+#pragma config VREGEN 	= OFF 				// No USB Voltage Regulation
+#pragma config PBADEN 	= OFF 				// No PORTB ADC
+#pragma config DEBUG 	= OFF 				// No debugging :(
 
 
 #define INPUT 		1
 #define OUTPUT 		0
+
 #define LED_DIR 	TRISDbits.TRISD3
 #define LED_OFF 	PORTDbits.RD3 = 0
 #define LED_ON 		PORTDbits.RD3 = 1
@@ -47,20 +50,24 @@ void _low_ISR(void) {
 
 
 void main(void) {
+	// ADCON0 					= 0;
+	// ADCON1  				= 0;
+	// ADCON2 					= 0;
+
 	INTCON 					= 0;	// Interrupts Off
 	CMCON 					= 0x07; // Comparators Off
-	ADCON1 					= 0x06; // Port as Digital IO (TODO: port?)
-	PORTEbits.RDPU 			= 0;  	// Disable pull ups (TODO: which ones?)
+	ADCON1bits.PCFG 		= 0x0F; // Digital inputs!
+	PORTEbits.RDPU 			= 0;  	// Disable pull ups
 	CCP1CON 				= 0; 	// P1B, P1C, P1D assigned as port pins
 
 	LED_DIR 				= OUTPUT;
 
+	int x = 0b0011;
+
 	while(1) {
 		LED_ON;
-		delay10ktcy(5);
-		// delay(5000);
+		delay100ktcy(15);
 		LED_OFF;
-		delay10ktcy(5);
-		// delay(5000);
+		delay100ktcy(15);
 	}
 }
